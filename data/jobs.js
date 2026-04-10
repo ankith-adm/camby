35df044c511abec2cf3894542f7e8ad006335592
ankith-adm <ankith@tec-leaders.com>
2026-04-09 20:16:29 -0500

    Create jobs.js

---
+++ data/jobs.js
@@ -0,0 +1,8 @@
+const jobsData = [
+  ["LinkedIn",     "https://www.linkedin.com/jobs/"],
+  ["Dice",         "https://www.dice.com/"],
+  ["Monster",      "https://www.monster.com/"],
+  ["Glassdoor",    "https://www.glassdoor.com/Job/index.htm"],
+  ["Indeed",       "https://www.indeed.com/"],
+  ["ZipRecruiter", "https://www.ziprecruiter.com/"]
+];
