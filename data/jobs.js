25d6a3a375d384ef59dd5873c520984f69c84c01
ankith-adm <ankith@tec-leaders.com>
2026-06-19 10:46:41 -0500

    Delete data directory

--- data/jobs.js
+++
@@ -1,8 +0,0 @@
-const jobsData = [
-  ["LinkedIn",     "https://www.linkedin.com/jobs/"],
-  ["Dice",         "https://www.dice.com/"],
-  ["Monster",      "https://www.monster.com/"],
-  ["Glassdoor",    "https://www.glassdoor.com/Job/index.htm"],
-  ["Indeed",       "https://www.indeed.com/"],
-  ["ZipRecruiter", "https://www.ziprecruiter.com/"]
-];
